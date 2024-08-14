671a35f82808fdc8cf9e62957153e38808b40dc4
Franklin Neves Filho <88294924+franklinnevesfilho@users.noreply.github.com>
2024-08-14 10:28:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="franklin_fastapi_extension",
-    version='0.0.14',
+    version='1.0.14',
     packages=find_packages(),
     install_requires=[
         "fastapi",
