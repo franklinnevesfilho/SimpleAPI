7ae0f2a1ba5e3bf9458770f35a3c02f7d87937cb
frank <franklin.neves.filho@gmail.com>
2024-08-14 09:28:58 -0400

    updated github actions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="franklin_fastapi_extension",
-    version="0.2.4",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "fastapi",
